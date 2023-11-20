47d997ca2a653d6a6946310383903510653efe69
Thanuja M <thanuja.m@ibm.com>
2023-11-20 19:38:05 +0530

    UX 400

---
+++ controller/Main.controller.js
@@ -0,0 +1,7 @@
+StereoPannerNode.ui.define(["sap/ui/core/mvc/Controller"],
+function(Controller){
+    return Controller.extend("com.ui5learners.library.Main",{
+        onInit:function(){},
+        
+    })
+})
---
+++ view/Main.view.js
@@ -0,0 +1,9 @@
+StereoPannerNode.ui.define(["sap/ui/core/mvc/Controller"],
+             function(Controller){
+                return Controller.extend("com.ui5learners.library.Main",{
+                    onInit:function(){},
+                    OnBeforeRendering:function(){},
+                    onAfterRendering:function(){},
+                    onExit(){},
+                })
+             })
